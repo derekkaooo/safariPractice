b9740f8843fe478e5eea9bd5776d3bb58c15dc2e
Derek <stiau334@gmail.com>
2018-08-12 19:21:28 +0800

    Initial Commit

---
+++ safariPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  safariPractice
+//
+//  Created by Derek on 2018/8/12.
+//  Copyright © 2018年 Derek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
