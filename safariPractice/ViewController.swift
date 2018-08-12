eb116de1d44e3cead1e544fd0e85eab08f4230bf
Derek <stiau334@gmail.com>
2018-08-12 19:51:58 +0800

    new

--- safariPractice/ViewController.swift
+++ safariPractice/ViewController.swift
@@ -7,9 +7,22 @@
 //
 
 import UIKit
+import SafariServices
+//ㄑ匯入SafariServices
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
+
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if let url = URL(string: "https://medium.com/"){
+            let safari = SFSafariViewController(url: url)
+            safari.delegate = self
+            present(safari, animated: true, completion: nil)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
